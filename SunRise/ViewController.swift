15c966f4d6c471f80a3a0cbbff21418c4c98df19
kimhao97 <nguyenkimhaodn@gmail.com>
2021-02-05 16:58:48 +0700

    Init Project

---
+++ SunRise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SunRise
+//
+//  Created by Hao Kim on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
